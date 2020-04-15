080c885e97cbfebbad958972d913964044d57433
Benjamin Shelton <Bshel419@rocketmail.com>
2020-04-15 13:01:43 -0500

    Beginning of seq version

---
+++ TrapSeq.c
@@ -0,0 +1,32 @@
+#include<stdlib.h>
+#include<math.h>
+
+float findArea(float b1, float b2);
+float sine(float num);
+float cosine(float num);
+float exponent(float num, int ex);
+
+int main()
+{
+    return 0;
+}
+
+float findArea(float h, float b1, float b2)
+{
+    return h * ((b1 + b2) / 2);
+}
+
+float sine(float num)
+{
+    return sin(num);
+}
+
+float cosine(float num)
+{
+    return cos(num);
+}
+
+float xSquared(float num, int ex)
+{
+    return pow(x, ex);
+}
